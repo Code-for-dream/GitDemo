19fb188c93f2f5aae8a38ebf74eb1232be16f15b
Code-for-dream <1442314632@qq.com>
2022-08-16 14:18:34 +0800

    second commit

--- src/main/java/com/zyx/git/GitTest.java
+++ src/main/java/com/zyx/git/GitTest.java
@@ -7,5 +7,6 @@ package com.zyx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 }
