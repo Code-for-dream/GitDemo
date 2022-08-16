de2b01634aaffe427b97813698a8f705abbbf8b7
Code-for-dream <1442314632@qq.com>
2022-08-16 15:09:55 +0800

    hot-fix commit2

--- src/main/java/com/zyx/git/GitTest.java
+++ src/main/java/com/zyx/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git!(hot-fix)");
+        System.out.println("hot-fix test");
     }
 }
