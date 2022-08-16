8b24dbce7d26ac8ce9f03d92179590b70bb8b897
小码虫 <62652700+Code-for-dream@users.noreply.github.com>
2022-08-16 17:16:35 +0800

    pull commit

--- src/main/java/com/zyx/git/GitTest.java
+++ src/main/java/com/zyx/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
