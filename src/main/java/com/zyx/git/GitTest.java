27f298c27df68abc09069a4bc1344ca52724dbfd
Code-for-dream <1442314632@qq.com>
2022-08-16 14:17:15 +0800

    first commit

---
+++ src/main/java/com/zyx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zyx.git;
+
+/**
+ * @author 骑着蜗牛ひ追导弹'
+ * @date 2022-08-16 14:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
