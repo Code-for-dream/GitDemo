993e5063add6337be4f4eee13a623e71af87a15d
Code-for-dream <1442314632@qq.com>
2022-08-16 14:19:19 +0800

    third commit

--- src/main/java/com/zyx/git/GitTest.java
+++ src/main/java/com/zyx/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 }
