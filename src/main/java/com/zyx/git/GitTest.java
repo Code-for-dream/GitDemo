5cd26f46332053ae87c29f04ae43e7254bac249c
Code-for-dream <1442314632@qq.com>
2022-08-16 16:51:24 +0800

    push test commit

--- src/main/java/com/zyx/git/GitTest.java
+++ src/main/java/com/zyx/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello Git!(hot-fix)");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
